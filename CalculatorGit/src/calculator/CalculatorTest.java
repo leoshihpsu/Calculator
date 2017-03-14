8a96ed60caefb2df3ec459feb63eb993680840fe
Krystian Piekarczyk <krystian.piekarczyk.praca@gmail.com>
2017-03-14 20:47:20 +0100

    Test's

--- CalculatorGit/src/calculator/CalculatorTest.java
+++ CalculatorGit/src/calculator/CalculatorTest.java
@@ -9,7 +9,18 @@ public class CalculatorTest {
 	@Test
 	public void test() {
 		assertEquals(1, new Calculator().calculate("2+2"));
-		
+	}
+		@Test
+		public void test1() {
+			assertEquals(1, new Calculator().calculate("2-5"));
+	}
+		@Test
+		public void test3() {
+			assertEquals(1, new Calculator().calculate("25/5"));
+	}
+		@Test
+		public void test2() {
+			assertEquals(1, new Calculator().calculate("12*5"));
 	}
 	
 
